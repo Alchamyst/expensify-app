860a549a1c1e12fd254c155631e90b5e3ebf800c
Alchamyst <ashley@ashley-yetman.co.uk>
2020-07-25 13:45:43 +0100

    add date picker + filtering to dashboard.

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,13 @@
+import moment from 'moment';
+
+
 // Filters reducer
 
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
 };
 
 export default (state = filtersReducerDefaultState, action) => {
