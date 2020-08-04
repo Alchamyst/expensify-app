85f332e3397bfc0c24e4e4725c7549b61de2e54d
Alchamyst <ashley@ashley-yetman.co.uk>
2020-08-04 18:32:54 +0100

    add tests for expenses reducer. moved test data for tests into separate fixtures directory.

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,66 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+    const expense = {
+        id: '4',
+        description: 'Netflix',
+        note: '',
+        amount: 2000,
+        createdAt: 0
+    };
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, expense ])
+});
+
+test('should edit an expense', () => {
+    const amount = 5000;
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[2].id,
+        updates: { 
+            amount 
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[2].amount).toBe(amount);
+});
+
+test('should not edit expense if expense not found', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: '-1',
+        updates: { 
+            amount: 10 
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
