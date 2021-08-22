c42af56969f1da570d85d40de51a869de71cdea8
Alchamyst <ashley@ashley-yetman.co.uk>
2021-08-22 15:07:20 +0100

    add login page with googleAuth function

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,8 +13,12 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+googleAuthProvider.setCustomParameters({
+  'prompt':'select_account'
+});
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // //child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
+import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
@@ -12,9 +13,10 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/" component={LoginPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
+                <Route path="/dashboard" component={ExpenseDashboardPage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
