a3fc9b726665d2ebab300d2bb679b4137b664069
Alchamyst <ashley@ashley-yetman.co.uk>
2021-08-27 13:39:23 +0100

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,22 +4,21 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
                 <Route path="/" component={LoginPage} exact={true} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
